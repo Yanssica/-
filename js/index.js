374b4f310d21957e98c8662aa6f260047e1e306d
tzl <3300541386@qq.com>
2020-12-14 22:44:51 +0800

    tzldg

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-(function() {
-    $(document).ready(function() {
+(function () {
+    $(document).ready(function () {
         $('#partial-view').partialViewSlider();
     });
-    $(document).ready(function() {
+    $(document).ready(function () {
         var partialView = $('#partial-view').partialViewSlider();
-        $('#prev').on('click', function() {
+        $('#prev').on('click', function () {
             partialView.prev();
         });
-        $('#next').on('click', function() {
+        $('#next').on('click', function () {
             partialView.next();
         });
     });
-    var arr = [];
-    $.get("http://192.168.1.94:3000/play/new", function(data) {
-        arr = data;
-    }, "json");
-    $(".t_jz").click(function() {
+    $(".t_jz").click(function () {
         $(".t_jz_div").css("display", "none");
         $(".t_jz_img").css("display", "block");
     })
-    $(".l_dl").click(function() {
-        location = "login.html"
+    $(".l_dl").click(function () {
+        $(".l_box").css("display", "block");
+    });
+    $(".l_zc").click(function () {
+        location = "registerpage.html"
+    });
+    $(".t_box_img").click(function () {
+        location = "index.html";
     })
 })();
